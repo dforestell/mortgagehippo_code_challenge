1be1d9008e04175e75b0c4dd88eb2b60be94c242
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2018-11-16 02:03:46 -0600

    add error handing for making withdrawls and define make deposits method with error handling.  Logic set for email alert too

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,23 +6,33 @@ class TransactionsController < ApplicationController
 
 	def make_withdrawl
 		@withdrawl = Transaction.new(transaction_type: "Withdrawl", coin_type: params[:coin_type], user_id: 1)
-		p params
 		@coin = Coin.find_by(name: params[:coin_type])
-		if @coin.quantity < 1 
-			render json: {status: "error", code: 4000, message: "Sorry but there are no #{params[:coin_type]}'s in the Machine Right Now"}
+		if @coin.nil?
+			render json: {status: "error", code: 4000, message: "Sorry but this machine does not deal with that type of coin currently"}
+		
 		else
-			@coin.decrement!(:quantity)
-			@withdrawl.save
-			render json: @withdrawl, status: :ok
+			if @coin.quantity < 1
+				render json: {status: "error", code: 4000, message: "Sorry but there are no #{params[:coin_type]}'s in the Machine Right Now"}
+			else
+				@coin.decrement!(:quantity)
+				@withdrawl.save
+				render json: @withdrawl, status: :ok
+			end
 		end
 
 		# alert_admins if @coin.quantity < 4 
-
-
-
 	end
 
 	def make_deposit
+		@deposit = Transaction.new(transaction_type: "Deposit", coin_type: params[:coin_type], user_id: 1)
+		@coin = Coin.find_by(name: params[:coin_type])
+		if @coin.nil?
+			render json: {status: "error", code: 4000, message: "Sorry but this machine does not deal with that type of coin currently"}
+		else
+			@coin.increment!(:quantity)
+			@deposit.save
+			render json: @deposit, status: :ok
+		end
 	end
 
 
